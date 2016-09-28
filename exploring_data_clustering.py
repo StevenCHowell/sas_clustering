1fb0a47b2fa3a4d2f07f4c9035328202c51d9387
Steven C. Howell <steven.howell@nist.gov>
2016-09-28 16:22:14 -0400

    flake8 compliant

--- exploring_data_clustering.py
+++ exploring_data_clustering.py
@@ -1,23 +1,43 @@
 
 # coding: utf-8
-
+'''
 # # Exploring Data Clustering
 #
 # Approaches to explore:
-# - [Priciple Component Analysis](https://en.wikipedia.org/wiki/Principal_component_analysis): [scikit-learn](http://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_3d.html)
-# - [Hierarchical Clustering](https://en.wikipedia.org/wiki/Hierarchical_clustering): [scikit-learn](http://scikit-learn.org/stable/modules/clustering.html) [DBSCAN](http://scikit-learn.org/stable/modules/clustering.html#dbscan)
-#
-#
+Priciple Component Analysis
+https://en.wikipedia.org/wiki/Principal_component_analysis
+http://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_3d.html
 
-# ### Notes on [DBSCAN](http://scikit-learn.org/stable/modules/clustering.html#dbscan):
-# There are two parameters to the algorithm, `min_samples` and `eps`, which define formally what we mean when we say dense. Higher `min_samples` or lower `eps` indicate higher density necessary to form a cluster.
-#
-# More formally, we define a core sample as being a sample in the dataset such that there exist `min_samples` other samples within a distance of `eps`, which are defined as neighbors of the core sample. This tells us that the core sample is in a dense area of the vector space. A cluster is a set of core samples, that can be built by recursively by taking a core sample, finding all of its neighbors that are core samples, finding all of their neighbors that are core samples, and so on. A cluster also has a set of non-core samples, which are samples that are neighbors of a core sample in the cluster but are not themselves core samples. Intuitively, these samples are on the fringes of a cluster.
-#
-# Any core sample is part of a cluster, by definition. Further, any cluster has at least min_samples points in it, following the definition of a core sample. For any sample that is not a core sample, and does have a distance higher than eps to any core sample, it is considered an outlier by the algorithm.
+# Hierarchical Clustering
+https://en.wikipedia.org/wiki/Hierarchical_clustering
+http://scikit-learn.org/stable/modules/clustering.html
 
-# ### Import and manipulate SAS data
-#
+DBSCAN
+http://scikit-learn.org/stable/modules/clustering.html#dbscan
+
+There are two parameters to the algorithm, `min_samples` and `eps`,
+which define formally what we mean when we say dense. Higher `min_samples`
+or lower `eps` indicate higher density necessary to form a cluster.
+
+More formally, we define a core sample as being a sample in the dataset
+such that there exist `min_samples` other samples within a distance of
+`eps`, which are defined as neighbors of the core sample. This tells us
+that the core sample is in a dense area of the vector space. A cluster
+is a set of core samples, that can be built by recursively by taking a
+core sample, finding all of its neighbors that are core samples, finding
+all of their neighbors that are core samples, and so on. A cluster also
+has a set of non-core samples, which are samples that are neighbors of
+a core sample in the cluster but are not themselves core samples.
+Intuitively, these samples are on the fringes of a cluster.
+
+Any core sample is part of a cluster, by definition. Further, any cluster
+has at least min_samples points in it, following the definition of a core
+sample. For any sample that is not a core sample, and does have a distance
+higher than eps to any core sample, it is considered an outlier by the
+algorithm.
+'''
+
+# Import and manipulate SAS data
 
 import glob
 import errno
@@ -26,10 +46,8 @@ import os.path as op
 import numpy as np
 from sklearn.preprocessing import RobustScaler
 from sklearn.cluster import DBSCAN
-from sklearn import metrics
+# from sklearn import metrics
 import sasmol.sasmol as sasmol
-
-
 
 
 def mkdir_p(path):
@@ -49,13 +67,13 @@ def mkdir_p(path):
 # In[2]:
 
 
-#~~~~~ Tune these parameters to adjust clustering ~~~~~#
+# ~~~~~ Tune these parameters to adjust clustering ~~~~~ #
 
-distance = 0.5   # radius in hyerdimensional space (increasing r reduces number of unique structures)
+distance = 0.5   # radius in hyerdimensional space
 min_samples = 2  # minimum number of structures to form a cluster
 q_max = 0.2    # maximum q value for clustering
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 out_dir = 'output'
 mkdir_p(out_dir)
@@ -90,8 +108,10 @@ saxs_data.append(first_data[1:, 1])
 for saxs_file in saxs_files[1:]:
     x_data = np.loadtxt(saxs_file)
     x_data = x_data[q_mask]
-    assert np.allclose(x_data[0, 1], first_data[0, 1]), 'ERROR: data not normalize'
-    assert np.allclose(x_data[:, 0], first_data[:, 0]), 'ERROR: data not on same Q-grid'
+    assert np.allclose(x_data[0, 1], first_data[0, 1]), (
+        'ERROR: data not normalized')
+    assert np.allclose(x_data[:, 0], first_data[:, 0]), (
+        'ERROR: data not on same Q-grid')
     saxs_data.append(x_data[1:, 1])
 saxs_data = np.array(saxs_data)
 
@@ -107,8 +127,10 @@ sans_data.append(first_data[1:, 1])
 for sans_file in sans_files[1:]:
     n_data = np.loadtxt(sans_file)
     n_data = n_data[q_mask]
-    assert np.allclose(n_data[0, 1], first_data[0, 1]), 'ERROR: data not normalize'
-    assert np.allclose(n_data[:, 0], first_data[:, 0]), 'ERROR: data not on same Q-grid'
+    assert np.allclose(n_data[0, 1], first_data[0, 1]), (
+        'ERROR: data not normalize')
+    assert np.allclose(n_data[:, 0], first_data[:, 0]), (
+        'ERROR: data not on same Q-grid')
     sans_data.append(n_data[1:, 1])
 sans_data = np.array(sans_data)
 
@@ -123,7 +145,7 @@ sans_data = np.array(sans_data)
 
 # In[7]:
 
-# n_samples, n_features = saxs_data.shape # for PCA, should be (n_samples, n_features)
+# n_samples, n_features = saxs_data.shape # should be (n_samples, n_features)
 # print('samples: {}\nfeatures: {}'.format(n_samples, n_features))
 
 
@@ -150,7 +172,8 @@ sans_data = np.array(sans_data)
 
 
 # #### Rescale the data
-# Originally used `StandardScaler` but changed to `RobustScaler` to avoid complications from outliers (which skew the mean)
+# Originally used `StandardScaler` but changed to `RobustScaler` to avoid
+# complications from outliers (which skew the mean)
 
 # In[11]:
 
@@ -247,13 +270,13 @@ print('performing DBSCAN clustering')
 x_db = DBSCAN(eps=distance, min_samples=min_samples).fit(scaled_saxs)
 x_core_samples_mask = np.zeros_like(x_db.labels_, dtype=bool)
 x_core_samples_mask[x_db.core_sample_indices_] = True
-x_labels = x_db.labels_ + 1 # 0's are independent groups
+x_labels = x_db.labels_ + 1  # 0's are independent groups
 x_clusters_ = len(set(x_labels)) - (1 if -1 in x_labels else 0)
 
 n_db = DBSCAN(eps=distance, min_samples=min_samples).fit(scaled_saxs)
 n_core_samples_mask = np.zeros_like(n_db.labels_, dtype=bool)
 n_core_samples_mask[n_db.core_sample_indices_] = True
-n_labels = n_db.labels_ + 1 # 0's are independent groups
+n_labels = n_db.labels_ + 1  # 0's are independent groups
 n_clusters_ = len(set(n_labels)) - (1 if -1 in n_labels else 0)
 
 
@@ -286,7 +309,9 @@ if len(x_labels) < 200:
 
 np.savetxt(op.join(out_dir, 'x_clusters.txt'), x_labels, fmt='%d')
 np.savetxt(op.join(out_dir, 'n_clusters.txt'), n_labels, fmt='%d')
-print('cluster info saved to {}: x_clusters.txt and n_clusters.txt'.format(out_dir))
+print('cluster info saved to {}/x_clusters.txt and {}/n_clusters.txt'.format(
+    out_dir, out_dir))
+
 
 # In[41]:
 
@@ -332,12 +357,12 @@ for i0 in range(40):
 
 print('opening dcd file for structure extraction')
 dcd_fname = glob.glob('*.dcd')
-assert len(dcd_fname) == 1, 'ERROR: unsure which dcd file to use: {}'.format(dcd_fname)
+assert len(dcd_fname) == 1, 'ERROR: multiple dcd files: {}'.format(dcd_fname)
 dcd_fname = dcd_fname[0]
 
 print('opening pdb file for atom information')
 pdb_fname = glob.glob('*.pdb')
-assert len(pdb_fname) == 1, 'ERROR: unsure which dcd file to use: {}'.format(pdb_fname)
+assert len(pdb_fname) == 1, 'ERROR: multiple pdb files: {}'.format(pdb_fname)
 pdb_fname = pdb_fname[0]
 
 
@@ -359,13 +384,13 @@ labels = n_labels
 # create a dcd for every cluster with >1 frame
 print('opening dcd for for storing unique structures')
 dcd_fnames = []
-cluster_out_files = [] # dcds for clusters
+cluster_out_files = []  # dcds for clusters
 unique_out_fname = op.join(out_dir, '{}_uniue.dcd'.format(dcd_fname[:-4]))
-dcd_out_file = mol.open_dcd_write(unique_out_fname) # dcd file for unique structures
+dcd_out_file = mol.open_dcd_write(unique_out_fname)
 
 dcd_in_file = mol.open_dcd_read(dcd_fname)
 
-print('opening {} dcds for storing the clustered structres'.format(len(unique)))
+print('opening {} dcds for the clustered structres'.format(len(unique)))
 for i in xrange(len(unique)):
     this_fname = op.join(out_dir, '{}_c{:05d}.dcd'.format(dcd_fname[:-4], i+1))
     dcd_fnames.append(this_fname)
@@ -385,7 +410,8 @@ for (i, label) in enumerate(labels):
         cluster_out_frame[label-1] += 1
         # print('adding frame to cluster {}'.format(label-1))
         # print(cluster_out_frame)
-        mol.write_dcd_step(cluster_out_files[label-1], 0, cluster_out_frame[label-1])
+        mol.write_dcd_step(cluster_out_files[label-1], 0,
+                           cluster_out_frame[label-1])
         if label not in visited_cluster:
             visited_cluster.add(label)
             dcd_out_frame += 1
