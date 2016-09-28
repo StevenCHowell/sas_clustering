70616ec79c3b56cf7fada95ee419cf223926af63
Steven C. Howell <steven.howell@nist.gov>
2016-09-28 17:21:04 -0400

    create separate output directory for each n_dcd

--- test/find_dcd_open_limit.py
+++ test/find_dcd_open_limit.py
@@ -21,14 +21,15 @@ pdb_fname = 'centered_mab.pdb'
 run_dir = 'output'
 n_dcds_list = [50, 500, 1000, 2000, 3000, 4500]
 
-mkdir_p(run_dir)
-
 mol = sasmol.SasMol(0)
 mol.read_pdb(pdb_fname)
 dcd_fnames = []
 cluster_out_files = []
 
 for n_dcds in n_dcds_list:
+    this_run = '{}_{}'.format(run_dir, n_dcds)
+    mkdir_p(this_run)
+
     print('creating {} dcds'.format(n_dcds))
     for i in xrange(n_dcds):
         index = i+1
@@ -38,7 +39,7 @@ for n_dcds in n_dcds_list:
             print(index)
 
 
-        this_fname = op.join(run_dir, '{}_c{:05d}.dcd'.format('prefix', i+1))
+        this_fname = op.join(this_run, '{}_c{:05d}.dcd'.format('prefix', i+1))
         dcd_fnames.append(this_fname)
         cluster_out_files.append(mol.open_dcd_write(dcd_fnames[i]))
 
