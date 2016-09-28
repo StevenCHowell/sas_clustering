0d3485d0b5adf1f7c0def2b29468d78ecf6ee581
Steven C. Howell <steven.howell@nist.gov>
2016-09-28 17:15:02 -0400

    add mkdir_p

--- test/find_dcd_open_limit.py
+++ test/find_dcd_open_limit.py
@@ -1,10 +1,28 @@
 import sasmol.sasmol as sasmol
 import numpy as np
+import os
 import os.path as op
+import errno
+
+def mkdir_p(path):
+    '''
+    make directory recursively
+    adapted from http://stackoverflow.com/questions/600268/
+    '''
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and op.isdir(path):
+            pass
+        else:
+            raise
 
 pdb_fname = 'centered_mab.pdb'
 run_dir = 'output'
 n_dcds_list = [50, 500, 1000, 2000, 3000, 4500]
+
+mkdir_p(run_dir)
+
 mol = sasmol.SasMol(0)
 mol.read_pdb(pdb_fname)
 dcd_fnames = []
@@ -15,7 +33,7 @@ for n_dcds in n_dcds_list:
     for i in xrange(n_dcds):
         index = i+1
         n_print = np.int(n_dcds/10)
-        
+
         if not index%n_print:
             print(index)
 
@@ -25,5 +43,5 @@ for n_dcds in n_dcds_list:
         cluster_out_files.append(mol.open_dcd_write(dcd_fnames[i]))
 
     for cluster_out_file in cluster_out_files:
+        mol.write_dcd_step(cluster_out_file, 0, 1)
         mol.close_dcd_write(cluster_out_file)
-        
